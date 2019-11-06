0e1abe00fb4197c78eb71405217878a6071283ba
aekik1118 <aekik1118@naver.com>
2019-11-06 15:37:39 +0900

    mini batch 로 학습 방법 변경

--- train.py
+++ train.py
@@ -9,9 +9,11 @@ INPUT_ROW_SIZE = te.TextEmbedding.INPUT_ROW_SIZE
 INPUT_COL_SIZE = te.TextEmbedding.INPUT_COL_SIZE
 TRAIN_DATA_SIZE = 100
 TEST_DATA_SIZE = 500
+batch_size = 100
+training_epochs = 3
 
 SAVE_MODEL = 'model/model_test_500_test.ckpt'
-INPUT_DATA_FILE_NAME = "data/korean_spoiler_splitted.csv"
+INPUT_DATA_FILE_NAME = "data/korean_spoiler_splitted_mini.csv"
 
 textEmbedding = te.TextEmbedding(INPUT_DATA_FILE_NAME)
 
@@ -24,9 +26,14 @@ train_step = tf.train.AdamOptimizer(0.005).minimize(cnn.loss)
 with tf.Session() as sess:
     print("start..")
     sess.run(tf.global_variables_initializer())
-    for i in range(100):
-        sess.run(train_step, feed_dict={cnn.X: train_data, cnn.Y:train_labels})
-        if i%10 == 0:
-            print(sess.run(cnn.accuracy, feed_dict={cnn.X: test_data, cnn.Y: test_labels}))
+
+    for epoch in range(training_epochs):
+        batch_count = int(train_data.shape[0]/batch_size)
+        for i in range(batch_count):
+            batch_x, batch_y = train_data[i*batch_size:(i+1)*batch_size], train_labels[i*batch_size:(i+1)*batch_size]
+            sess.run(train_step, feed_dict={cnn.X: batch_x, cnn.Y: batch_y})
+
+        print(str(epoch+1)+':'+str(sess.run(cnn.accuracy, feed_dict={cnn.X: test_data, cnn.Y: test_labels})))
+
     saver = tf.train.Saver()
     saver.save(sess, SAVE_MODEL)
