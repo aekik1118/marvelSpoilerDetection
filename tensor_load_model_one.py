649b4ad85be929561c8528a33bbfdb4fe9516116
aekik1118 <aekik1118@naver.com>
2019-11-06 13:00:37 +0900

    KoreanCnn 객체 적용

--- tensor_load_model_one.py
+++ tensor_load_model_one.py
@@ -6,19 +6,15 @@ import random
 
 import text_embedding as te
 import korean_splitter as ks
+import korean_cnn as kc
 
 INPUT_ROW_SIZE = te.TextEmbedding.INPUT_ROW_SIZE
 INPUT_COL_SIZE = te.TextEmbedding.INPUT_COL_SIZE
 
 TEST_STR = "아이언11맨 사111111망"
-
-SAVE_MODEL = 'model/model_test_7500.ckpt'
-
-tf.disable_v2_behavior()
+SAVE_MODEL = 'model/model_test_4000_filter_6.ckpt'
 
 INPUT_DATA_FILE_NAME = "data/korean_spoiler_splitted_only_sp.csv"
-
-numpy.set_printoptions(threshold=sys.maxsize)
 
 textEmbedding = te.TextEmbedding(INPUT_DATA_FILE_NAME)
 
@@ -26,59 +22,10 @@ sp_data = ks.KoreanSplitter.split_korean(TEST_STR)
 
 test_data = textEmbedding.embedding_string(sp_data)
 
-X = tf.placeholder(tf.float32, [None, INPUT_ROW_SIZE, INPUT_COL_SIZE,1])
-Y = tf.placeholder(tf.float32, [None, 2])
-# filter_sizes = [3, 4, 5, 6, 7, 8]
-filter_sizes = [3, 4, 5]
-embedding_size = INPUT_COL_SIZE
-num_filters = 128
-sequence_length = INPUT_ROW_SIZE
-num_classes = 2
-l2_reg_lambda = 0.0
-l2_loss = tf.constant(0.0)
-# dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
-pooled_outputs = []
-for i, filter_size in enumerate(filter_sizes):
-    filter_shape = [filter_size, embedding_size, 1, num_filters]
-    W = tf.Variable(tf.truncated_normal(shape=filter_shape, stddev=0.1), name="W")
-    b = tf.Variable(tf.constant(0.1,shape=[num_filters]), name="b")
-    conv = tf.nn.conv2d(
-        X,
-        W,
-        strides=[1,1,1,1],
-        padding="VALID",
-        name="conv"
-    )
-    # Apply nonlinearity
-    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-    # Maxpooling over the outputs
-    pooled = tf.nn.max_pool(
-        h,
-        ksize=[1, sequence_length - filter_size + 1, 1, 1],
-        strides=[1, 1, 1, 1],
-        padding='VALID',
-        name="pool")
-    pooled_outputs.append(pooled)
-num_filters_total = num_filters * len(filter_sizes)
-h_pool = tf.concat(pooled_outputs, 3)
-h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
-# with tf.name_scope("dropout"):
-#     h_drop = tf.nn.dropout(h_pool_flat, dropout_keep_prob)
-with tf.name_scope("output"):
-    W = tf.get_variable(
-        "W",
-        shape=[num_filters_total, num_classes]
-        # initializer=tf.contrib.layers.xavier_initializer()
-    )
-    b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-    l2_loss += tf.nn.l2_loss(W)
-    l2_loss += tf.nn.l2_loss(b)
-    scores = tf.nn.xw_plus_b(h_pool_flat, W, b, name="scores")
-    predictions = tf.argmax(scores, 1, name="predictions")
-# Combine all the pooled features
+cnn = kc.KoreanCNN(INPUT_ROW_SIZE, INPUT_COL_SIZE)
 
 with tf.Session() as sess:
     print("load data...")
     saver = tf.train.Saver()
     saver.restore(sess, SAVE_MODEL)
-    print(sess.run(predictions, feed_dict={X: test_data}))
+    print(sess.run(cnn.predictions, feed_dict={cnn.X: test_data}))
--- tensor_load_model_test.py
+++ tensor_load_model_test.py
@@ -5,85 +5,25 @@ import numpy
 import random
 
 import text_embedding as te
+import korean_cnn as kc
 
 INPUT_ROW_SIZE = te.TextEmbedding.INPUT_ROW_SIZE
 INPUT_COL_SIZE = te.TextEmbedding.INPUT_COL_SIZE
 TRAIN_DATA_SIZE = 1
-TEST_DATA_SIZE = 1000
+TEST_DATA_SIZE = 500
 
 SAVE_MODEL = 'model/model_test_4000_filter_6.ckpt'
 
-tf.disable_v2_behavior()
-
 INPUT_DATA_FILE_NAME = "data/korean_spoiler_splitted_only_sp.csv"
-
-numpy.set_printoptions(threshold=sys.maxsize)
 
 textEmbedding = te.TextEmbedding(INPUT_DATA_FILE_NAME)
 
 test_data, test_labels, train_data, train_labels = textEmbedding.embedding_data(TEST_DATA_SIZE, TRAIN_DATA_SIZE)
 
+cnn = kc.KoreanCNN(INPUT_ROW_SIZE, INPUT_COL_SIZE)
 
-X = tf.placeholder(tf.float32, [None, INPUT_ROW_SIZE, INPUT_COL_SIZE,1])
-Y = tf.placeholder(tf.float32, [None, 2])
-filter_sizes = [3, 4, 5, 6, 7, 8]
-# filter_sizes = [3, 4, 5]
-embedding_size = INPUT_COL_SIZE
-num_filters = 128
-sequence_length = INPUT_ROW_SIZE
-num_classes = 2
-l2_reg_lambda = 0.0
-l2_loss = tf.constant(0.0)
-# dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
-pooled_outputs = []
-for i, filter_size in enumerate(filter_sizes):
-    filter_shape = [filter_size, embedding_size, 1, num_filters]
-    W = tf.Variable(tf.truncated_normal(shape=filter_shape, stddev=0.1), name="W")
-    b = tf.Variable(tf.constant(0.1,shape=[num_filters]), name="b")
-    conv = tf.nn.conv2d(
-        X,
-        W,
-        strides=[1,1,1,1],
-        padding="VALID",
-        name="conv"
-    )
-    # Apply nonlinearity
-    h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-    # Maxpooling over the outputs
-    pooled = tf.nn.max_pool(
-        h,
-        ksize=[1, sequence_length - filter_size + 1, 1, 1],
-        strides=[1, 1, 1, 1],
-        padding='VALID',
-        name="pool")
-    pooled_outputs.append(pooled)
-num_filters_total = num_filters * len(filter_sizes)
-h_pool = tf.concat(pooled_outputs, 3)
-h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
-# with tf.name_scope("dropout"):
-#     h_drop = tf.nn.dropout(h_pool_flat, dropout_keep_prob)
-with tf.name_scope("output"):
-    W = tf.get_variable(
-        "W",
-        shape=[num_filters_total, num_classes]
-        # initializer=tf.contrib.layers.xavier_initializer()
-    )
-    b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-    l2_loss += tf.nn.l2_loss(W)
-    l2_loss += tf.nn.l2_loss(b)
-    scores = tf.nn.xw_plus_b(h_pool_flat, W, b, name="scores")
-    predictions = tf.argmax(scores, 1, name="predictions")
-# Combine all the pooled features
-losses = tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=Y)
-loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
-correct_predictions = tf.equal(predictions, tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
-# Loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=predictions))
-train_step = tf.train.AdamOptimizer(0.005).minimize(loss)
-# correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(Y,1))
-# accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 with tf.Session() as sess:
     print("load data...")
     saver = tf.train.Saver()
     saver.restore(sess, SAVE_MODEL)
-    print(sess.run(accuracy, feed_dict={X: test_data, Y: test_labels}))
+    print(sess.run(cnn.accuracy, feed_dict={cnn.X: test_data, cnn.Y: test_labels}))
